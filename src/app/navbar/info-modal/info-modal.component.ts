aa9a487d4961dc238c49d60c8c15f4fb1f39f27e
Finn Fiedler <finn.f@mailbox.org>
2019-11-22 20:10:10 +0100

    reworked info modal

--- src/app/navbar/info-modal/info-modal.component.ts
+++ src/app/navbar/info-modal/info-modal.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { DarkThemeService } from 'src/app/_services/implementations/darktheme.service';
 import { Subscription } from 'rxjs';
+import * as hljs from 'highlight.js';
+import * as sqlSyntax from 'highlight.js/lib/languages/SQL';
 
 @Component({
   selector: 'app-info-modal',
   templateUrl: './info-modal.component.html',
   styleUrls: ['./info-modal.component.sass']
 })
-export class InfoModalComponent implements OnDestroy {
+export class InfoModalComponent implements OnDestroy, AfterViewInit {
+  @ViewChild('code', {static: true})
+  codeElement: ElementRef;
 
   private darkThemeSubscription: Subscription;
   
@@ -16,6 +20,11 @@ export class InfoModalComponent implements OnDestroy {
 
     this.darkTheme = darkThemeService.getDarkThemeState();
     this.darkThemeSubscription = darkThemeService.darkThemeSubject.subscribe(() => this.darkTheme = !this.darkTheme);
+  }
+
+  ngAfterViewInit() {
+    //hljs.registerLanguage()
+    hljs.highlightBlock(this.codeElement.nativeElement);
   }
 
   ngOnDestroy() {
